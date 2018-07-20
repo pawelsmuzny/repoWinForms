d82f6e63db3c0e14460df7e6abce706139406f2e
Pawel Smuzny <pawelsmuzny@hotmail.com>
2018-07-20 20:07:25 +0200

    Init repo

---
+++ ProjektWinForms/Garib.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektWinForms
+{
+    public class Garib
+    {
+        public void spi()
+        {
+
+        }
+
+        public void chrapie()
+        {
+
+        }
+    }
+}
---
+++ ProjektWinForms/Pawel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektWinForms
+{
+    public class Pawel
+    {
+        public int RozmiarRolek { get; set; }
+
+
+        public void jezdziNaRolkach()
+        {
+
+        }
+
+        public void jezdziNaRowerze()
+        {
+
+        }
+
+    }
+}
